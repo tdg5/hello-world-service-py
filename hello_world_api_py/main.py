45d48c608633b53adc46aa5355943f93f074c5b9
Danny Guinther <dannyguinther@gmail.com>
2024-01-28 12:37:14 -0500

    Port Main logic to service_oriented.application.GenericMain

--- hello_world_api_py/main.py
+++ hello_world_api_py/main.py
@@ -1,22 +1,11 @@
-from typing import Optional
+from service_oriented.application import GenericMain
 
 from hello_world_api_py.app.application import Application
 from hello_world_api_py.app.config import Config
 
 
-class Main:
-    def __init__(self, config: Optional[Config] = None):
-        self.config = config or self._build_config()
-        self.application = self._build_application(self.config)
-
-    def _build_application(self, config: Config) -> Application:
-        return Application(config=config)
-
-    def _build_config(self) -> Config:
-        return Config()
-
-    def run(self) -> None:
-        self.application.run()
+class Main(GenericMain[Config, Application]):
+    pass
 
 
 if __name__ == "__main__":
